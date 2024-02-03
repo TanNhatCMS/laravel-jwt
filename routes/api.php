1da4211f222ea106f7aacdf1ac10766e008ca51c
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-02-03 15:00:28 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
-
 ], function ($router) {
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+    Route::get('/profile', [AuthController::class, 'userProfile']);
     Route::post('/change-pass', [AuthController::class, 'changePassWord']);
+
+    //Route::post('/update-profile', [AuthController::class, 'updateProfile']);
+    //Route::post('/update-avatar', [AuthController::class, 'updateAvatar']);
+    //Route::post('/update-cover', [AuthController::class, 'updateCover']);
+    //Route::post('/update-info', [AuthController::class, 'updateInfo']);
+    //Route::post('/update-social', [AuthController::class, 'updateSocial']);
+    //Route::post('/update-contact', [AuthController::class, 'updateContact']);
+    //Route::post('/update-education', [AuthController::class, 'updateEducation']);
+    //Route::post('/update-experience', [AuthController::class, 'updateExperience']);
+    //Route::post('/update-skill', [AuthController::class, 'updateSkill']);
+    //Route::post('/update-language', [AuthController::class, 'updateLanguage']);
+
 });
 
